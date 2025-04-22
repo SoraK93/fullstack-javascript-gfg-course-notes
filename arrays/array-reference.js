63dbf39cd703bd75d4d328e8605851decf6db868
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-04-22 08:33:11 +0530

    array reference lesson notes

---
+++ arrays/array-reference.js
@@ -0,0 +1,45 @@
+// Shallow Copy
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+
+// console.log(arr1);
+// console.log(arr2);
+
+// arr2.push(4);
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+// Spread Operator
+// let arr3 = [...arr1];
+
+// console.log(arr1);
+// console.log(arr3);
+
+// arr3.push(5);
+
+// console.log(arr1);
+// console.log(arr3);
+
+
+// for loop
+// let arr4 = [1, 2, 3, 4];
+// let arr5 = [];
+
+// for (let number of arr4) {
+//     arr5.push(number);
+// }
+
+// console.log(arr4);
+// console.log(arr5);
+
+// arr5.push(5);
+
+// console.log(arr4);
+// console.log(arr5);
+
+
+let str = "Sourabh";
+let arrOfAlpha = [...str];
+console.log(arrOfAlpha);
