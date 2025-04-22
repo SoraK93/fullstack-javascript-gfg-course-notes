7694c85cae27e9cc86a3d2d898f9a3daa7d6ff7e
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-04-22 08:49:44 +0530

    modified array reference notes

--- arrays/array-reference.js
+++ arrays/array-reference.js
@@ -40,6 +40,22 @@
 // console.log(arr5);
 
 
-let str = "Sourabh";
-let arrOfAlpha = [...str];
-console.log(arrOfAlpha);
+// using array.from
+// let arr6 = [1, 2, 3];
+// let arr7 = Array.from(arr6);
+// arr7.push(4);
+// console.log(arr6);
+// console.log(arr7);
+
+
+// Using concat
+let arr8 = [1, 2, 3];
+let arr9 = arr8.concat();
+arr9.push(4);
+console.log(arr8);
+console.log(arr9);
+
+
+// let str = "Sourabh";
+// let arrOfAlpha = [...str];
+// console.log(arrOfAlpha);
