5dde8ef3a7dd91f29618d3b8fa13f520de5ec2de
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-04-23 09:20:55 +0530

    OBJECT COMPUTED PROPRETIES LESSON NOTES

--- object/computed-properties.js
+++ object/computed-properties.js
@@ -3,16 +3,23 @@
  * Computed Property
  */
 
-const readlineSync = require("../arrays/node_modules/readline-sync");
-const key = readlineSync.question("What do you want to know about (name/age/state/city)?");
+const { read } = require("fs");
+const readlineSync = require("../node_modules/readline-sync");
 
+const key = readlineSync.question("What do you want to know about (name/age/state/city)? ");
+
+const course = readlineSync.question("Which course do you want to learn (html/css/js/react/redux)? ")
 
 const obj = {
     name: "Sourabh",
     age: 32,
+    get [course]() {
+        return `Lets start by finding a suitable ${course} course for you.`;
+    },
 }
 obj.city = "Kolkata";
 obj.state = "WB";
 
 console.log(obj[key]);
+console.log(obj[course]);
 
