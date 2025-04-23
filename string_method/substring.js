0dede822c028ba1bab6f90cea73691fa8e1bc867
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-04-23 06:26:55 +0530

    MODIFIED SUBSTRING NOTES WITH ADDITIONAL EXAMPLES

--- string_method/substring.js
+++ string_method/substring.js
@@ -11,4 +11,30 @@ function getDisplayName(userName, maxLength) {
 }
 
 const displayName = getDisplayName(userName, maxLength);
-console.log(displayName);
+// console.log(displayName);
+
+
+// first the first occurance of substring, without using in-built methods
+function firstOccurence(txt, pat) {
+  // code here
+  let indexTxt = 0;
+  let indexPat = 0;
+  
+  while (true) {
+      if (txt[indexTxt] !== pat[indexPat]) {
+          indexPat = 0;
+      } else if (indexPat === pat.length) {
+          break;
+      } else {
+        indexPat++;
+      }
+      
+      indexTxt++;
+      
+      if (indexTxt === txt.length) {
+          break;
+      }
+  }
+  return (indexPat === pat.length) ? indexTxt - indexPat : -1;
+}
+console.log(firstOccurence("nxtipemjbxualljkxgbbwmkxouqeyegvkildhxcqsfayer", "bwmkxouqeyegvkildhxcqsfayer"));
