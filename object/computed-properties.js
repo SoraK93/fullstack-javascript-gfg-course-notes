4825b1fcd78e8b11f8dd6f185b3c65f0018f82d3
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-04-23 08:46:23 +0530

    MODIFIED .GITIGNORE

--- object/computed-properties.js
+++ object/computed-properties.js
@@ -3,6 +3,16 @@
  * Computed Property
  */
 
+const readlineSync = require("../arrays/node_modules/readline-sync");
+const key = readlineSync.question("What do you want to know about (name/age/state/city)?");
 
 
+const obj = {
+    name: "Sourabh",
+    age: 32,
+}
+obj.city = "Kolkata";
+obj.state = "WB";
+
+console.log(obj[key]);
 
