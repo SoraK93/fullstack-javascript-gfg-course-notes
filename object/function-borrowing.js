35743f958f1a7e1bcf31e999ea36ae2eeba89904
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-04-26 07:07:31 +0530

    ADD CALL, APPLY AND BIND LESSON NOTES

---
+++ object/function-borrowing.js
@@ -0,0 +1,55 @@
+/**
+ * Function Borrowing - Call, Apply And Bind
+ */
+
+const user1 = {
+  name: "Sourabh",
+  age: 32,
+  // sayHi() {
+  //     console.log(this.name);
+  // },
+};
+const user2 = {
+  name: "Shambhu",
+  age: 70,
+  // sayHi() {
+  //     console.log(this.name);
+  // },
+};
+const user3 = {
+  name: "Urmila",
+  age: 65,
+  // sayHi() {
+  //     console.log(this.name);
+  // },
+};
+const user4 = {
+  name: "Sweta",
+  age: 35,
+  sayHi() {
+    // Implicit Binding
+    console.log(this.name);
+  },
+};
+
+// user1.sayHi(); // Doesnt work since no method called sayHi exists inside object
+
+function sayHi(degree, year) {
+  console.log(this.name, degree, year);
+}
+
+// Explicit Binding
+
+// Call takes simple arguments, when we have parameters to deal with.
+// sayHi.call(user1, "PGDM", 2017); // the value of 'this' is user1
+// sayHi.call(user2, "B.Com (HONS)", 2014); // the value of 'this' is user2
+
+// Apply works similar to call, it just takes a array of parameters.
+// sayHi.apply(user1, ["PDGM", 2017]);
+// sayHi.apply(user2, ["B.Com(HONS)", 2014]);
+
+// user4.sayHi.call(user3);
+
+// Bind works similar to above, but in this we need to declare the func to a var and call it using that var.
+const result = sayHi.bind(user1, "B.Com");
+result(2005);
