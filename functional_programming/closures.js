9126a1bc9f0dff39d3438604f39226294bba61c4
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-05-02 08:53:35 +0530

    MODIFY: ADD EXAMPLES TO CLOSURE LESSON NOTES

--- functional_programming/closures.js
+++ functional_programming/closures.js
@@ -1,11 +1,12 @@
 /**
  * A closure is the combination of a function bundled together with references to its lexical environment.
- * 
+ *
  * In other words, A closure is a function tat remembers its outer variables and can access them.
- * 
- * 
+ *
+ *
  */
 
+// Example-1
 let c;
 function x() {
     let a = 5;
@@ -26,3 +27,41 @@ function x() {
 }
 console.log(c);
 x();
+
+// Example-2
+// logs incorrect values
+function noClosure() {
+  let array = [];
+  for (var i = 0; i < 3; i++) {
+    array.push(function () {
+      console.log(i);
+    });
+  }
+  return array;
+}
+const invalidResult = noClosure();
+// At every iteration our value of i is changing and since we are using var, all the changes are pointing to same reference. And at the last iteration i = 3, is taken as final value.
+invalidResult[0](); // 3
+invalidResult[1](); // 3
+invalidResult[2](); // 3
+
+// Two ways to correct this error
+// Change var declaration to let. This is very simple, and will help function store distinct values of i during push.
+// Create a immediately invoked function expression (IIFE) during push. This uses the concept of closure to store distinct value of i for each iteration.
+function withClosure() {
+  let array = [];
+  for (var i = 0; i < 3; i++) {
+    array.push(
+      (function (i) {
+        return function () {
+          console.log(i);
+        };
+      })(i)
+    );
+  };
+  return array;
+}
+const validResult = withClosure();
+validResult[0]();
+validResult[1]();
+validResult[2]();
